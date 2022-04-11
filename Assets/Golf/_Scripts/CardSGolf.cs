d6494abf3043b24695fb57678c5f273576c7de77
teegc <tcurtis727@outlook.com>
2022-04-11 14:52:14 -0400

    new golf

--- Assets/Golf/_Scripts/CardSGolf.cs
+++ Assets/Golf/_Scripts/CardSGolf.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardSGolf : MonoBehaviour
+public enum eCardState_Golf
 {
-    // Start is called before the first frame update
-    void Start()
+    drawpile,
+    tableau,
+    target,
+    discard
+}
+
+public class CardSGolf : Card_Golf
+{
+    [Header("Set Dynamically: CardSGolf")]
+    public eCardState_Golf state = eCardState_Golf.drawpile;
+    public List<CardSGolf> hiddenBy = new List<CardSGolf>();
+    public int layoutID;
+    public SlotDef_Golf slotDef;
+
+    public override void OnMouseUpAsButton()
     {
-        
+        SGolf.S.CardClicked(this); 
+        base.OnMouseUpAsButton();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
