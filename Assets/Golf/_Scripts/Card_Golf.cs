5c5f20e54f5910261e6deda037227809fd0a4219
teegc <tcurtis727@outlook.com>
2022-04-11 12:59:23 -0400

    set lay

---
+++ Assets/Golf/_Scripts/Card_Golf.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Card_Golf : MonoBehaviour {
+    [Header("Set Dynamically")]
+    public string suit;
+    public int rank;
+    public Color color = Color.black;
+    public string colS = "Black";
+    public List<GameObject> decoGOs = new List<GameObject>();
+    public List<GameObject> pipGOs = new List<GameObject>();
+    public GameObject back;
+    public CardDefinition def;
+    public SpriteRenderer[] spriteRenderers;
+    public bool isGold;
+
+    void Start()
+    {
+        SetSortOrder(0);
+    }
+
+    public void PopulateSpriteRenderers()
+    {
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    public void SetSortOrder(int sOrd)
+    {
+        PopulateSpriteRenderers();
+
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                tSR.sortingOrder = sOrd;
+                continue;
+            }
+
+            switch (tSR.gameObject.name)
+            {
+                case "back":
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+
+                case "face":
+                default:
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
+
+    public bool faceUp
+    {
+        get { return !back.activeSelf; }
+        set { back.SetActive(!value);}
+    }
+
+    public virtual void OnMouseUpAsButton()
+    {
+        print(name);
+    }
+}
+
+[System.Serializable]
+public class Decorator_Golf
+{
+    public string type;
+    public Vector3 loc;
+    public bool flip = false;
+    public float scale = 1f;
+}
+
+[System.Serializable]
+public class CardDefinition_Golf
+{
+    public string face;
+    public int rank;
+    public List<Decorator> pips = new List<Decorator>();
+}
